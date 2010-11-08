0b5c36f060b83c17fdf2bf11fd85e1bee0cadd76
William Trevor Olson <wtolson@gmail.com>
2010-11-08 12:53:58 -0700

    Updated ChangeLog.  Added gpl license and acknowledgments.

--- js/pioneerwoman.js
+++ js/pioneerwoman.js
@@ -1,9 +1,25 @@
-/************************
+/***********************************************************************
  * 
- * By Jim
+ *     This file is part of gRecipes.
+ *
+ *     gRecipes is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     gRecipes is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
  * 
- ***********************/
-GR_DEBUG = true;
+ *     You should have received a copy of the GNU General Public License
+ *     along with gRecipes.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ *     Support for The Pioneer Woman brought to you by Jim <cutiger02@gmail.com>
+ * 
+ **********************************************************************/
+ 
+//GR_DEBUG = true;
 
 function getTitle() {
     return document.title;
