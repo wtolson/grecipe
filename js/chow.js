1c05e1eda068b0aa19227be6cd39291afad18cde
William Trevor Olson <wtolson@gmail.com>
2010-11-08 11:40:50 -0700

    Forgot to actually add the files.

---
+++ js/chow.js
@@ -0,0 +1,44 @@
+//GR_DEBUG = true;
+
+function getTitle() {
+    return $('#title h1').text().trim();
+}
+
+function getSummary() {
+	var ans = "<div>Via <a href='http://www.chow.com/'>CHOW</a></div>";
+	
+	if($('#intro_full').size() != 0) {
+		var intro = $('#intro_full');
+		$('p', intro).each( function() {
+			$(this).append('<br>');
+		});
+		ans += "<br><div>" +  $('#intro_full').html() + '</div>';
+	}
+	
+	return ans;
+}
+
+function getIngredients() {
+	var ans = "";
+	
+	if($('#ingredients').size() != 0) {
+		ans += $('#ingredients').html();
+	}
+	
+	return ans;
+}
+
+function getInstructions() {
+	inst = $('#instructions').clone();
+	$('ol', inst).each( function() {
+		$(this).replaceWith($(this).html());
+	});
+	$('li', inst).each( function() {
+		$(this).replaceWith('<p>' + $(this).html() + '</p><br>');
+	});
+    return inst.html();
+}
+
+function pageHasRecipe () {
+    return ( $('body').hasClass('recipes_show') );
+}
---
+++ js/foodnetwork.js
@@ -0,0 +1,36 @@
+//GR_DEBUG = true;
+
+function getTitle() {
+    return $('h1.fn').text().trim();
+}
+
+function getSummary() {
+	return "<div>Via <a href='http://www.foodnetwork.com/'>Food Network</a></div>";
+}
+
+function getIngredients() {
+	ingredients = $('div.body-text').clone();
+	$('h2', ingredients).each( function() {
+			if ( $(this).text() == 'Ingredients' || $(this).text() == 'Directions') {
+				$(this).remove();
+			} else {
+				$(this).replaceWith('<h3>' + $(this).text() + '</h3>');
+			}
+	});
+	$('script, div.instructions, div.next-recipe', ingredients).each( function() {
+			$(this).remove()
+	});
+    return ingredients.html();
+}
+
+function getInstructions() {	
+	ans = '';
+	$('.instructions').each( function () {
+		ans += $(this).html();
+	});
+    return ans;
+}
+
+function pageHasRecipe () {
+    return ( $('body').hasClass('recipe-detail') );
+}
